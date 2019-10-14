b0e5417ee5b260d23995d55600037ebf8417314d
coxtor <contact@vcranz.de>
2019-10-14 12:34:36 +0200

    Plugin compatibility with SDK 28, fix Gradle and migrate to androidx

--- app/src/main/java/ryey/easeroperationpluginexample/MainActivity.java
+++ app/src/main/java/ryey/easeroperationpluginexample/MainActivity.java
@@ -19,8 +19,9 @@
 
 package ryey.easeroperationpluginexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
